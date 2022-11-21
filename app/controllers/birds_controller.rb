76fefc51a59500f9875d252499b7bc9ec3d7a68b
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-21 22:29:12 +0300

    complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,4 @@
 class BirdsController < ApplicationController
-
   # GET /birds
   def index
     birds = Bird.all
@@ -16,4 +15,8 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
 end
